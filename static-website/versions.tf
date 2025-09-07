bf8e377993bd26a955d2c2f06f95c2a14ef0cc02
Ben <ben.larson@hotmail.com>
2025-09-07 16:55:19 +0000

    Fix deprecation warning

--- static-website/versions.tf
+++ static-website/versions.tf
@@ -15,7 +15,7 @@ data "aws_region" "us_east_1_provider" {
 resource "null_resource" "guard" {
   lifecycle {
     precondition {
-      condition     = data.aws_region.us_east_1_provider.name == "us-east-1"
+      condition     = data.aws_region.us_east_1_provider.region == "us-east-1"
       error_message = "The supplied aws.us_east_1 provider must be for us-east-1."
     }
   }
