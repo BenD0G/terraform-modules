9a813a960871b4c9107ca1d73d591ef61defc478
Ben <ben.larson@hotmail.com>
2025-09-07 16:50:45 +0000

    Add check for us-east-1

--- static-website/versions.tf
+++ static-website/versions.tf
@@ -7,3 +7,16 @@ terraform {
     }
   }
 }
+
+data "aws_region" "us_east_1_provider" {
+  provider = aws.us_east_1
+}
+
+resource "null_resource" "guard" {
+  lifecycle {
+    precondition {
+      condition     = data.aws_region.us_east_1_provider.name == "us-east-1"
+      error_message = "The supplied aws.us_east_1 provider must be for us-east-1."
+    }
+  }
+}
