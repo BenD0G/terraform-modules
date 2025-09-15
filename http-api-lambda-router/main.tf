08d1c26f7c318913da50325ebe69c5e765a502fa
Ben <ben.larson@hotmail.com>
2025-09-15 17:00:15 +0000

    Try fixing error

--- http-api-lambda-router/main.tf
+++ http-api-lambda-router/main.tf
@@ -21,17 +21,28 @@ resource "aws_acm_certificate" "api" {
   validation_method = "DNS"
 }
 
+# One DNS record per validation option
 resource "aws_route53_record" "acm_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.api.domain_validation_options :
+    dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+
   zone_id = data.aws_route53_zone.root.zone_id
-  name    = aws_acm_certificate.api.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.api.domain_validation_options[0].resource_record_type
-  records = [aws_acm_certificate.api.domain_validation_options[0].resource_record_value]
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.value]
   ttl     = 60
 }
 
+# Wait for all validations
 resource "aws_acm_certificate_validation" "api" {
   certificate_arn         = aws_acm_certificate.api.arn
-  validation_record_fqdns = [aws_route53_record.acm_validation.fqdn]
+  validation_record_fqdns = [for r in aws_route53_record.acm_validation : r.value.fqdn]
 }
 
 # ────────────────────────────────────────────────────────────────────────────────
